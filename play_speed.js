4642f3a46c16c69d4587d1d6ac02be4b0b738118
burn-mehs006 <mehs006@gmail.com>
2026-09-14 21:16:44 +0900

    CustomScript#3

--- play_speed.js
+++ play_speed.js
@@ -17,5 +17,34 @@
     // 例：動画タイトルを取得してログに出す
     wait('h1.title').then(el => {
         console.log("Video title:", el.innerText);
+
+        videoSkip();
     });
 })();
+
+
+function videoSkip() {
+    document.addEventListener('mousedown', function (e) {
+        const btn = e.target.closest('#efyt-custom-script');
+        if (!btn) {
+            return;
+        }
+
+        var player = document.querySelector('video');
+        if (!player) return;
+
+        let time = 0;
+        if (e.button === 0) {
+            time = 5;
+        } else if (e.button === 1) {
+            time = -10;
+        } else if (e.button === 2) {
+            time = -5;
+        } else if (e.button === 3) {
+            time = -5;
+        } else if (e.button === 4) {
+            time = 5;
+        }
+        player.currentTime += time;
+    });
+}
