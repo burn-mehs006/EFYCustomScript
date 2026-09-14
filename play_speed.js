f233656363656ed6302b7865468bd4dfc281f4ff
burn-mehs006 <mehs006@gmail.com>
2026-09-14 20:42:26 +0900

    CustomScrip#2

--- play_speed.js
+++ play_speed.js
@@ -1,18 +1,21 @@
 (function () {
     console.log("Enhancer custom script loaded");
 
+    let count = 1;
     const wait = (selector) => new Promise(resolve => {
         const timer = setInterval(() => {
             const el = document.querySelector(selector);
             if (el) {
+                console.log("intervalCount:", count);
                 clearInterval(timer);
                 resolve(el);
             }
+            count++;
         }, 500);
     });
 
     // 例：動画タイトルを取得してログに出す
-    wait('h1.title').then(title => {
-        console.log("Video title:", title.innerText);
+    wait('h1.title').then(el => {
+        console.log("Video title:", el.innerText);
     });
 })();
