add206a23bec0f0ae58167d3fde505718800a5bb
burn-mehs006 <mehs006@gmail.com>
2026-09-14 20:31:01 +0900

    CustomScript#1

--- play_speed.js
+++ play_speed.js
@@ -1,3 +1,18 @@
 (function () {
-    console.log("Githubからファイルを読み込んで実行できた！！！！");
+    console.log("Enhancer custom script loaded");
+
+    const wait = (selector) => new Promise(resolve => {
+        const timer = setInterval(() => {
+            const el = document.querySelector(selector);
+            if (el) {
+                clearInterval(timer);
+                resolve(el);
+            }
+        }, 500);
+    });
+
+    // 例：動画タイトルを取得してログに出す
+    wait('h1.title').then(title => {
+        console.log("Video title:", title.innerText);
+    });
 })();
